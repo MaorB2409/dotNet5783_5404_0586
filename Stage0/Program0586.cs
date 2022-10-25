d3efc363e306121dcc99103256abd9f9df1ef1b4
Maor Bokobza <maor3017@gmail.com>
2022-10-25 22:19:05 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program0586.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stage0 // Note: actual namespace depends on the project name.
+{
+     partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0586();
+            Welcome5404();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        static partial void Welcome5404();
+
+        private static void Welcome0586()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application!", name);
+        }
+    }
+}
