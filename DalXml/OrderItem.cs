0386b1c7781d3d4521d59b0940ccc64edafd911e
shana <shoshana.kayman@gmail.com>
2023-01-08 13:31:42 +0200

    dalxml classes and functions for all three entities

--- DalXml/OrderItem.cs
+++ DalXml/OrderItem.cs
@@ -8,46 +8,108 @@ using System.Threading.Tasks;
 namespace Dal;
 using DalApi;
 using DO;
+using System.Xml.Linq;
 
 internal class OrderItem : IOrderItem
 {
+    string orderItemPath = @"OrderItem.xml";
     public int Add(DO.OrderItem item)
     {
-        throw new NotImplementedException();
+        XElement orderItemRoot = XmlTools.LoadListFromXMLElement(orderItemPath); //get all the elements from the file
+
+        //check if the orderItem exists in th file
+        var orderItemFromFile = (from oi in orderItemRoot.Elements()
+                                 where (oi != null && oi.Element("ID")!.Value == item.ID.ToString())
+                                 select oi).FirstOrDefault();
+
+        //throw an exception
+        if (orderItemFromFile != null)
+            throw new DalApi.IdExistException("the order already exists");
+
+        //add the orderItem to the root element
+        orderItemRoot.Add(
+            new XElement("OrderItem",
+            new XElement("ID", item.ID),
+            new XElement("ProductID", item.ProductID),
+            new XElement("OrderID", item.OrderID),
+            new XElement("Price", item.Price),
+            new XElement("Amount", item.Amount)));
+
+        //save the root in the file
+        XmlTools.SaveListToXMLElement(orderItemRoot, orderItemPath);
+        return item.ID;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        List<DO.OrderItem?> orderItemList = XmlTools.LoadListFromXMLSerializer<DO.OrderItem?>(orderItemPath);
+
+        DO.OrderItem temp = (from item in orderItemList
+                             where item != null && item?.ID == id
+                             select (DO.OrderItem)item).FirstOrDefault();
+
+        if (temp.ID.Equals(default(Order)))
+            throw new DalApi.IdNotExistException("the product does not exist");
+
+        orderItemList.Remove(temp);
+
+        XmlTools.SaveListToXMLSerializer(orderItemList, orderItemPath);
     }
 
     public IEnumerable<DO.OrderItem?> GetAll(Func<DO.OrderItem?, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        List<DO.OrderItem?> orderItemList = XmlTools.LoadListFromXMLSerializer<DO.OrderItem?>(orderItemPath).ToList();
+
+        return (from order in orderItemList
+                where filter(order)
+                select order).ToList();
     }
 
     public DO.OrderItem GetByFilter(Func<DO.OrderItem?, bool>? filter)
     {
-        throw new NotImplementedException();
+        List<DO.OrderItem?> orderItemList = GetAll().ToList();
+
+        return (from item in orderItemList
+                where filter(item)
+                select (DO.OrderItem)item).FirstOrDefault();
     }
 
     public DO.OrderItem GetById(int id)
     {
-        throw new NotImplementedException();
+        List<DO.OrderItem?> orderItemList = GetAll().ToList();
+
+        return (from item in orderItemList
+                where item != null && item?.ID == id
+                select (DO.OrderItem)item).FirstOrDefault();
+        throw new DalApi.IdNotExistException("the Order Item requested does not exist");
     }
 
     public DO.OrderItem ItemOfOrder(int id, int productId)
     {
-        throw new NotImplementedException();
+        List<DO.OrderItem?> orderItemList = GetAll().ToList();
+
+        return (from item in orderItemList
+                where item != null && item?.ProductID == productId && item?.OrderID==id
+                select (DO.OrderItem)item).FirstOrDefault();
+        throw new DalApi.IdNotExistException("the Order Item requested does not exist");
     }
 
     public IEnumerable<DO.OrderItem?> ItemsInOrder(int id)
     {
-        throw new NotImplementedException();
+        List<DO.OrderItem?> orderItemList = GetAll().ToList();
+
+        return (from item in orderItemList
+                where item?.OrderID == id
+                select item).ToList();
     }
 
     public void Update(DO.OrderItem item)
     {
-        throw new NotImplementedException();
+        DO.OrderItem? temp = GetById(item.ID);//get the order item requested to update 
+        List<DO.OrderItem?> orderItemList = GetAll().ToList();//get all order items from ile
+        orderItemList.Remove(temp);//remove the existing order item
+        orderItemList.Add(item);//add the updated order item
+
+        XmlTools.SaveListToXMLSerializer(orderItemList, orderItemPath);//save back into file    
     }
 }
--- DalXml/Product.cs
+++ DalXml/Product.cs
@@ -7,36 +7,89 @@ using System.Threading.Tasks;
 namespace Dal;
 using DalApi;
 using DO;
+using System.Xml.Linq;
 
 internal class Product : IProduct
 {
+    string productPath = @"Product.xml";
     public int Add(DO.Product item)
     {
-        throw new NotImplementedException();
+        XElement productRoot = XmlTools.LoadListFromXMLElement(productPath); //get all the elements from the file
+
+        //check if the customer exists in th file
+        var customerFromFile = (from prod in productRoot.Elements()
+                                where (prod.Element("ID").Value == item.ID.ToString())
+                                select prod).FirstOrDefault();
+
+        //throw an exception
+        if (customerFromFile != null)
+            throw new DalApi.IdExistException("the product already exists");
+
+        //add the customer to the root element
+        productRoot.Add(
+            new XElement("Product",
+            new XElement("ID", item.ID),
+            new XElement("Name", item.Name),
+            new XElement("Price", item.Price),
+            new XElement("Category", item.Category),
+            new XElement("InStock", item.InStock)));
+
+        //save the root in the file
+        XmlTools.SaveListToXMLElement(productRoot, productPath);
+        return item.ID;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        List<DO.Product?> productList = XmlTools.LoadListFromXMLSerializer<DO.Product?>(productPath);
+
+        DO.Product temp = (from item in productList
+                           where item != null && item?.ID == id
+                           select (DO.Product)item).FirstOrDefault();
+
+        if (temp.ID.Equals(default(Order)))
+            throw new DalApi.IdNotExistException("the product does not exist");
+
+        productList.Remove(temp);
+
+        XmlTools.SaveListToXMLSerializer(productList, productPath);
     }
 
     public IEnumerable<DO.Product?> GetAll(Func<DO.Product?, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        List<DO.Product?> productList = XmlTools.LoadListFromXMLSerializer<DO.Product?>(productPath).ToList();
+
+        return (from product in productList
+                where filter(product)
+                select product).ToList();
     }
 
     public DO.Product GetByFilter(Func<DO.Product?, bool>? filter)
     {
-        throw new NotImplementedException();
+        List<DO.Product?> productList = GetAll().ToList();
+
+        return (from item in productList
+                where filter(item)
+                select (DO.Product)item).FirstOrDefault();
     }
 
     public DO.Product GetById(int id)
     {
-        throw new NotImplementedException();
+        List<DO.Product?> productList = GetAll().ToList();
+
+        return (from item in productList
+                where item != null && item?.ID == id
+                select (DO.Product)item).FirstOrDefault();
+        throw new DalApi.IdNotExistException("the product requested does not exist");
     }
 
     public void Update(DO.Product item)
     {
-        throw new NotImplementedException();
+        DO.Product? temp = GetById(item.ID);//get the product requested to update 
+        List<DO.Product?> productList = GetAll().ToList();//get all product from ile
+        productList.Remove(temp);//remove the existing product
+        productList.Add(item);//add the updated product
+
+        XmlTools.SaveListToXMLSerializer(productList, productPath);//save back into file    }
     }
 }
